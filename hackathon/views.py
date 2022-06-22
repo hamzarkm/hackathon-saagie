5176cff421e2e589e939d2cf5a9fe8d78173db33
hamza <hamza.chaouai@gmail.com>
2022-06-23 01:13:08 +0200

    add request package

--- hackathon/views.py
+++ hackathon/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
 
 # Create your views here.
 
+def getToken():
+    url = "https://demo-workspace.a4.saagie.io/authentication/api/open/authenticate"
+    headers = {'content-type': 'application/json','Saagie-Realm':'demo'}
+    r = requests.post(url, headers=headers, data={'login': 'ESTIAM_G18_manuel.dassi-kueti','password': 'QAmvjE9TSU'})
+    print(r.text)
+
+
 def index(request):
-    return HttpResponse("Hello, world. You're at the hackathon index.")
+    getToken()
+    return HttpResponse("welcome")
+
+ 
